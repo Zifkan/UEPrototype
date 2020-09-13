eeca521d06b31126ec019d45cad9597628bcb291
Ylly <zifkanishe@gmail.com>
2020-09-13 21:51:45 +0300

    Just temp commit

---
+++ Source/UEPrototype/Animations/FCharacterAnimEditorModule.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleInterface.h"
+#include "SkeletalMeshEditor/Public/ISkeletalMeshEditorModule.h"
+
+class FCharacterAnimEditorModule : public IModuleInterface
+{
+public:
+
+    void StartupModule() override;
+    void ShutdownModule() override;
+
+
+
+    void AddCharacterAnimEditorExtender();
+    void RemoveCharacterAnimEditorExtender();
+
+    FDelegateHandle ModuleLoadedDelegateHandle;
+    FDelegateHandle SkeletalMeshEditorExtenderHandle;
+    TSharedRef<FExtender> GetCharacterAnimEditorExtender(const TSharedRef<FUICommandList> CommandList, TSharedRef<ISkeletalMeshEditor> InSkeletalMeshEditor);
+    void HandleAddCharacterAnimEditorExtenderToToolbar(FToolBarBuilder& ParentToolbarBuilder, UDebugSkelMeshComponent* InMeshComponent);
+
+private:
+
+
+    void ConvertToMesh(UDebugSkelMeshComponent* PreviewComponent);
+
+    const char* moduleNameConst = "SkeletalMeshEditor";
+
+};
--- Source/UEPrototype/UEPrototype.Build.cs
+++ Source/UEPrototype/UEPrototype.Build.cs
@@ -11,6 +11,14 @@ public class UEPrototype : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay"});
 
+        PrivateIncludePathModuleNames.AddRange(
+            new string[] {
+                "GraphEditor",
+                "SkeletalMeshEditor",
+                // ... add other private include paths required here ...
+            }
+        );
+
         PublicIncludePaths.AddRange(new string[]
         {
             "UEPrototype/ThirdParty",
@@ -18,12 +26,40 @@ public class UEPrototype : ModuleRules
             "UEPrototype/Private",
             "UEPrototype/ECS",
             "UEPrototype/Converters",
+            "UEPrototype/Animations",
             /* Path.Combine(ThirdPartyPath, "ECSPlugin", "Source", "ECSPlugin","Public"),
              Path.Combine(ThirdPartyPath, "ECSPlugin", "Source", "ECSPlugin","Private"),
              Path.Combine(ThirdPartyPath, "ECSPlugin", "Source", "ECSPlugin","ECS"),
              Path.Combine(ThirdPartyPath, "ECSPlugin", "Source", "ECSPlugin","ECS","Systems"),
              Path.Combine(ThirdPartyPath, "ECSPlugin", "Source", "ECSPlugin","ECS","Systems","Transforms"),*/
         });
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Projects",
+                "TargetPlatform",
+                "MeshDescription",
+                "MeshDescriptionOperations",
+
+                "CoreUObject",
+                "Engine",
+                "RawMesh",
+                "Slate",
+                "SlateCore",
+                "UnrealEd",
+                "EditorStyle",
+                "RHI",
+                "SlateCore",
+                "Slate",
+                "RenderCore",
+                "AnimGraph",
+                "AnimationBlueprintEditor",
+                "AnimationEditor",
+                "SkeletalMeshEditor",
+                // ... add private dependencies that you statically link with here ...	
+            }
+        );
     }
 
     private string ThirdPartyPath
