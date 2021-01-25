b592aeee6aa0561147470d374fca8e3ad35109a5
Ylly <Zifkan@mail.ru>
2021-01-25 22:40:42 +0300

    Fix Player input direction

--- Source/UEPrototype/ECS/Systems/Movement/PlayerViewDirectionSystem.cpp
+++ Source/UEPrototype/ECS/Systems/Movement/PlayerViewDirectionSystem.cpp
@@ -5,14 +5,31 @@
 
 void PlayerViewDirectionSystem::OnCreate()
 {
-    SystemRun->each([this](Entity e, ViewDirectionData& viewDirection)
+    SystemRun->signature("PlayerTag").each([this](Entity e, ViewDirectionData& viewDirection)
     {
         const auto s = m_pWorld->DefaultWorld->singleton<CamEntityType>();
         auto* camLocalToWorld = s.get<WorldToLocal>();
 
         if (camLocalToWorld ==nullptr) return;
-        
-        viewDirection.Axises = camLocalToWorld->Forward();       
+
+
+    	auto forward = camLocalToWorld->Forward();
+        auto right = camLocalToWorld->Right();
+ 
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.Z = 0.f;
+        right.Z = 0.f;    
+     
+    	forward = forward.ProjectOnToNormal(FVector::UpVector);   
+    	forward.Normalize();
+	
+        //this is the direction in the world space we want to move:
+        auto desiredMoveDirection = forward  + right;
+
+
+
+    	
+         viewDirection.Axises = desiredMoveDirection;       
        
     }).kind(0);    
 }
